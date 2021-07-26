004975e7f65e42184599b4068889337ff6deaf62
Neha Gopinathan <nehagopinathan18@gmail.com>
2021-07-26 05:36:40 +0530

    changed score value calculation on resubmit.

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -8,12 +8,12 @@ const correctAns = [
 const ques = document.querySelectorAll('.ques');
 const form1 = document.querySelector('form');
 const output = document.querySelector('.output');
-console.log(form1);
-let score = 0;
+
+
 form1.addEventListener('submit', (e) => {
     e.preventDefault();
     let form_data = new FormData(form1);
-   
+    let score = 0;
     let index = 0;
     for (let value of form_data) {
         
